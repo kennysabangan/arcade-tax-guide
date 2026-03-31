d89b2407b70201b10f9c2919a02e772b0961771c
Kenneth Sabangan <scalesolving@Kenneths-Mac-mini.local>
2026-03-31 16:55:12 -0700

    Install and configure @vercel/analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import { Layout } from './components/Layout'
 import HomePage from './pages/HomePage'
 import TaxGuidePage from './pages/TaxGuidePage'
@@ -22,6 +23,7 @@ export default function App() {
         </Route>
         <Route path="analytics" element={<AnalyticsPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }
